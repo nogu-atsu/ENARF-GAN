caa086a57a95628487a3765987a84a0030c1a6b3
nogu-atsu <noguchi@mi.t.u-tokyo.ac.jp>
2021-10-03 21:02:55 +0900

    refactor

--- train_encoder_supervised.py
+++ train_encoder_supervised.py
@@ -140,64 +140,65 @@ def train_func(config, datasets, data_loaders, rank, ddp=False, world_size=1):
             start_time = snapshot["start_time"]
             del snapshot
 
-    while iter < num_iter:
-        for i, minibatch in enumerate(loader_img):
-            if (iter + 1) % 10 == 0 and rank == 0:
-                print(f"{iter + 1} iter, {(time.time() - start_time) / iter} s/iter")
-            enc.train()
+    for i, minibatch in enumerate(loader_img):
+        if (iter + 1) % 10 == 0 and rank == 0:
+            print(f"{iter + 1} iter, {(time.time() - start_time) / iter} s/iter")
+        enc.train()
 
-            real_img = minibatch["img"].cuda(non_blocking=True).float()
-            pose_2d = minibatch["pose_2d"].cuda(non_blocking=True).float()
-            pose_3d = minibatch["pose_3d"].cuda(non_blocking=True).float()
+        real_img = minibatch["img"].cuda(non_blocking=True).float()
+        pose_2d = minibatch["pose_2d"].cuda(non_blocking=True).float()
+        pose_3d = minibatch["pose_3d"].cuda(non_blocking=True).float()
 
-            enc_optimizer.zero_grad()
+        enc_optimizer.zero_grad()
 
-            # reconstruction
-            pose_3d_enc, _, _, _ = enc(real_img, pose_2d)  # (B, n_parts, 4, 4), (B, z_dim*4), (B, n_parts)
-            scaled_pose_3d = enc.scale_pose(pose_3d[:, :, :3, 3:])
+        # reconstruction
+        pose_3d_enc, _, _, _ = enc(real_img, pose_2d)  # (B, n_parts, 4, 4), (B, z_dim*4), (B, n_parts)
+        scaled_pose_3d = enc.scale_pose(pose_3d[:, :, :3, 3:])
 
-            loss_dict = {}
-            loss_rotation = mse(pose_3d[:, :, :3, :3], pose_3d_enc[:, :, :3, :3])
-            loss_translation = mse(scaled_pose_3d, pose_3d_enc[:, :, :3, 3:])
+        loss_dict = {}
+        loss_rotation = mse(pose_3d[:, :, :3, :3], pose_3d_enc[:, :, :3, :3])
+        loss_translation = mse(scaled_pose_3d, pose_3d_enc[:, :, :3, 3:])
 
-            loss = loss_rotation + loss_translation
-            loss_dict["loss_rotation"] = loss_rotation
-            loss_dict["loss_translation"] = loss_translation
+        loss = loss_rotation + loss_translation
+        loss_dict["loss_rotation"] = loss_rotation
+        loss_dict["loss_translation"] = loss_translation
 
-            if rank == 0:
-                if iter % 100 == 0:
-                    print(iter)
-                    for k, v in loss_dict.items():
-                        write(iter, v, k, writer, True)
+        if rank == 0:
+            if iter % 100 == 0:
+                print(iter)
+                for k, v in loss_dict.items():
+                    write(iter, v, k, writer, True)
 
-            loss.backward()
-            enc_optimizer.step()
+        loss.backward()
+        enc_optimizer.step()
 
-            torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
 
-            if rank == 0:
-                if iter == 10:
-                    with open(f"{out_dir}/result/{out_name}/iter_10_succeeded.txt", "w") as f:
-                        f.write("ok")
-                if (iter + 1) % 1000 == 0:
-                    loss_dict_val = evaluate(enc, test_loader_img)
-                    for k, v in loss_dict_val.items():
-                        write(iter, v, k, writer, True)
+        if rank == 0:
+            if iter == 10:
+                with open(f"{out_dir}/result/{out_name}/iter_10_succeeded.txt", "w") as f:
+                    f.write("ok")
+            if (iter + 1) % 1000 == 0:
+                loss_dict_val = evaluate(enc, test_loader_img)
+                for k, v in loss_dict_val.items():
+                    write(iter, v, k, writer, True)
 
-                if (iter + 1) % 200 == 0:
-                    if ddp:
-                        enc_module = enc.module
-                    else:
-                        enc_module = enc
-                    save_params = {"iteration": iter,
-                                   "start_time": start_time,
-                                   "enc": enc_module.state_dict(),
-                                   "enc_opt": enc_optimizer.state_dict()}
-                    torch.save(save_params, f"{out_dir}/result/{out_name}/snapshot_latest.pth")
-                    torch.save(save_params, f"{out_dir}/result/{out_name}/snapshot_{(iter // 50000 + 1) * 50000}.pth")
+            if (iter + 1) % 200 == 0:
+                if ddp:
+                    enc_module = enc.module
+                else:
+                    enc_module = enc
+                save_params = {"iteration": iter,
+                               "start_time": start_time,
+                               "enc": enc_module.state_dict(),
+                               "enc_opt": enc_optimizer.state_dict()}
+                torch.save(save_params, f"{out_dir}/result/{out_name}/snapshot_latest.pth")
+                torch.save(save_params, f"{out_dir}/result/{out_name}/snapshot_{(iter // 5000 + 1) * 5000}.pth")
 
-            torch.cuda.empty_cache()
-            iter += 1
+        torch.cuda.empty_cache()
+        iter += 1
+        if iter > num_iter:
+            break
 
 
 if __name__ == "__main__":
