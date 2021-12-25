dc00d12ec3c6e51f20f0b088bb16072a37e99811
nogu-atsu <noguchi@mi.t.u-tokyo.ac.jp>
2021-12-25 15:48:31 +0900

    probablistic encoder (without check)

--- models/net.py
+++ models/net.py
@@ -1,13 +1,14 @@
 import math
+from typing import Optional
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 import torchvision
 from torch import nn
-import torch.nn.functional as F
 
-from NARF.models.net import NeRF
 from NARF.models.model_utils import whole_image_grid_ray_sampler
+from NARF.models.net import NeRF
 from models.stylegan import Generator as StyleGANGenerator
 from models.stylegan import StyledConv, ModulatedConv2d
 from utils.rotation_utils import rotation_6d_to_matrix
@@ -262,6 +263,152 @@ class Encoder(nn.Module):
                                         pose_homo[:, :, :, None])  # (B, n_parts, 3, 1)
         pose_translation = self.scale_pose(pose_translation)
 
+        pose_Rt = torch.cat([rotation_matrix, pose_translation], dim=-1)  # (B, n_parts, 3, 4)
+        num_parts = pose_Rt.shape[1]
+        pose_Rt = torch.cat([pose_Rt,
+                             torch.tensor([0, 0, 0, 1],
+                                          device=pose_Rt.device)[None, None, None].expand(batchsize, num_parts, 1, 4)],
+                            dim=2)
+
+        # bone length
+        coordinate = pose_Rt[:, :, :3, 3]
+        length = torch.linalg.norm(coordinate[:, 1:] - coordinate[:, self.parents[1:]], axis=2)
+        bone_length = length[:, :, None]
+        return pose_Rt, z, bone_length, intrinsic
+
+
+class ProbablisticEncoder(nn.Module):
+    def __init__(self, config, parents: np.ndarray):
+        super(ProbablisticEncoder, self).__init__()
+        self.image_encoder = torchvision.models.resnet18(pretrained=True)
+        self.register_buffer('resnet_mean', torch.tensor([0.485, 0.456, 0.406]))
+        self.register_buffer('resnet_std', torch.tensor([0.229, 0.224, 0.225]))
+        self.image_feat_dim = config.image_feat_dim
+        self.image_size = config.image_size
+        self.z_dim = config.z_dim
+        self.num_bone = parents.shape[0]
+        self.n_pe_freq = config.n_positional_encoding_freq
+
+        transformer_hidden_dim = config.transformer_hidden_dim
+        transformer_n_head = config.transformer_n_head
+        num_encoder_layers = config.num_encoder_layers
+
+        encoder_layer = nn.TransformerEncoderLayer(d_model=transformer_hidden_dim, nhead=transformer_n_head)
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+
+        self.pose_positional_embedding = nn.Parameter(torch.randn(self.num_bone, 1,
+                                                                  transformer_hidden_dim))
+
+        self.linear_image = nn.Linear(self.image_feat_dim, transformer_hidden_dim * self.num_bone)
+        self.linear_pose = nn.Linear(2, transformer_hidden_dim)
+        self.linear_z_pose = nn.Linear(self.z_dim, transformer_hidden_dim * self.num_bone)
+
+        self.linear_out = nn.Linear(transformer_hidden_dim, 7)
+
+        # z
+        self.linear_z1 = nn.Linear(self.image_feat_dim, self.image_feat_dim)
+        self.linear_z2 = nn.Linear(self.image_feat_dim, self.z_dim * 4)
+
+        # self.intrinsic = intrinsic.astype("float32")
+        self.parents = parents  # parent ids
+        self.mean_bone_length = 0.15  # mean length of bone. Should be calculated from dataset??
+
+    def positional_encoding(self, pose: torch.tensor) -> torch.tensor:
+        """
+        Apply positional encoding to pose
+        Args:
+            pose: (n_parts, B, 2)
+
+        Returns: (n_parts, B, 4L + 2)
+
+        """
+        encoded = [pose]
+        encoded += [torch.cos(pose * 2 ** i * np.pi) for i in range(self.n_pe_freq)]
+        encoded += [torch.sin(pose * 2 ** i * np.pi) for i in range(self.n_pe_freq)]
+        encoded = torch.cat(encoded, dim=-1)
+        return encoded
+
+    def scale_pose(self, pose):
+        bone_length = torch.linalg.norm(pose[:, 1:] - pose[:, self.parents[1:]], dim=2)[:, :, 0]  # (B, n_parts-1)
+        mean_bone_length = bone_length.mean(dim=1)
+        return pose / mean_bone_length[:, None, None, None] * self.mean_bone_length
+
+    def get_rotation_matrix(self, d6: torch.Tensor, pose_translation: torch.tensor) -> torch.Tensor:
+        """
+        Rotation of root joint -> x_axis = d6[:3]
+        Rotation of other joints -> x_axis = child_trans - parent_trans
+
+        Args:
+            d6: 6D rotation representation, of size (n_parts, B, 6)
+            pose_translation: (B, n_parts, 3, 1)
+        Returns:
+            batch of rotation matrices of size (*, 3, 3)
+        """
+        d6 = d6.permute(1, 0, 2)
+        a1, a2 = d6[..., :3], d6[..., 3:]
+        parent_child = pose_translation[:, 1:, :, 0] - pose_translation[:, self.parents[1:], :, 0]
+        a1 = torch.cat([a1[:, :1], parent_child], dim=1)  # (B, num_parts, 3)
+        b1 = F.normalize(a1, dim=-1)
+        b2 = a2[:, None] - (b1 * a2[:, None]).sum(-1, keepdim=True) * b1
+        b2 = F.normalize(b2, dim=-1)
+        b3 = torch.cross(b1, b2, dim=-1)
+        return torch.stack((b1, b2, b3), dim=-1)  # (B, num_parts, 3, 3)
+
+    def forward(self, img: torch.tensor, pose_2d: torch.tensor, intrinsic: Optional[torch.tensor],
+                z_pose: Optional[torch.tensor]):
+        """estimate 3d pose from image and GT 2d pose
+
+        Args:
+            img: (B, 3, 128, 128)
+            pose_2d: (B, n_parts, 2)
+            intrinsic:
+            z_pose: latent vector for pose
+
+        Returns:
+
+        """
+        intrinsic = torch.tensor(intrinsic, device=img.device)
+        inv_intrinsic = torch.inverse(intrinsic)[:, None]
+        batchsize = img.shape[0]
+
+        img = (img * 0.5 + 0.5 - self.resnet_mean) / self.resnet_std
+        image_feature = self.image_encoder(img)
+
+        # z
+        h = self.linear_z1(image_feature)  # (B, 512)
+        z = self.linear_z2(h)  # (B, z_dim * 4)
+
+        # pose
+        image_feature = self.linear_image(image_feature)  # (B, 512 * n_parts)
+        image_feature = image_feature.reshape(batchsize, -1, self.num_bone)
+        image_feature = image_feature.permute(2, 0, 1)  # (n_parts, B, -1)
+
+        normalized_pose_2d = pose_2d / (self.image_size / 2) - 1
+        encoded_pose = self.positional_encoding(normalized_pose_2d)
+        pose_feature = encoded_pose.permute(1, 0, 2)  # (n_parts, B, 4L+2)
+        pose_feature = self.linear_pose(pose_feature)  # (n_parts, B, -1)
+
+        if z_pose is None:
+            z_pose = torch.randn(batchsize, self.z_dim, device=img.device)
+        pose_latent = self.linear_z_pose(z_pose)
+        pose_latent = pose_latent.reshape(batchsize, -1, self.num_bone)
+        pose_latent = pose_latent.permute(2, 0, 1)  # (n_parts, B, -1)
+
+        # transformer input
+        pose_feature = pose_feature + self.pose_positional_embedding + pose_latent + image_feature
+
+        h = self.transformer(pose_feature)  # (n_parts, B, transformer_hidden_dim)
+        h = self.linear_out(h)  # (n_parts, B, 7)
+
+        # joint translation
+        depth = F.softplus(h[..., 0])  # (n_parts, B)
+        pose_homo = torch.cat([pose_2d, torch.ones_like(pose_2d[:, :, :1])], dim=2) * depth.permute(1, 0)[:, :, None]
+        pose_translation = torch.matmul(inv_intrinsic,
+                                        pose_homo[:, :, :, None])  # (B, n_parts, 3, 1)
+        pose_translation = self.scale_pose(pose_translation)
+
+        rot = h[..., 1:]  # (n_parts, B, 6)
+        rotation_matrix = self.get_rotation_matrix(rot, pose_translation)  # (B, n_parts, 3, 3)
         pose_Rt = torch.cat([rotation_matrix, pose_translation], dim=-1)  # (B, n_parts, 3, 4)
         num_parts = pose_Rt.shape[1]
         pose_Rt = torch.cat([pose_Rt,
