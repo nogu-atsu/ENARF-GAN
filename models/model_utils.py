a42161f65178d58c4a9c54bcac5d504dbe4b7bca
nogu-atsu <noguchi@mi.t.u-tokyo.ac.jp>
2022-02-17 21:29:47 +0900

    fix sampler

--- models/model_utils.py
+++ models/model_utils.py
@@ -21,20 +21,19 @@ def mask_based_sampler(mask: torch.Tensor, ray_batchsize: int
     batchsize, h, w = mask.shape
 
     # expand mask
-    pad_size = h // 16
+    pad_size = h // 8
     mask = F.max_pool2d(mask.float(), pad_size * 2 + 1, stride=1, padding=pad_size)
     mask = mask.reshape(batchsize, h * w)
 
     # random sample
-    mask = mask + torch.rand_like(mask)
+    mask = mask + torch.empty_like(mask).uniform_()
     ray_idx = torch.topk(mask, ray_batchsize, dim=1, sorted=False)[1]
     x = ray_idx % w  # (B, ray_bs)
     y = ray_idx // w  # (B, ray_bs)
 
-    rays = torch.stack([x, y], dim=2)[None] + 0.5
-    grid = rays / (h / 2) - 1
+    rays = torch.stack([x, y], dim=2) + 0.5
 
     rays = rays.permute(0, 2, 1)
     homo_img = torch.cat([rays, torch.ones(batchsize, 1, ray_batchsize, device="cuda")], dim=1)  # B x 3 x n
     homo_img = homo_img.reshape(batchsize, 1, 3, -1)
-    return grid, homo_img
+    return ray_idx, homo_img
