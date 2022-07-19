f726131bf7b5d27bb32a38f5e15a5153aaef531e
nogu-atsu <noguchi@mi.t.u-tokyo.ac.jp>
2022-07-19 20:57:30 +0900

    :sparkles: prepare canonical pose

---
+++ data_preprocess/prepare_canonical_pose.py
@@ -0,0 +1,18 @@
+import sys
+from smplx.body_models import SMPL
+import numpy as np
+import torch
+
+sys.path.append("../")
+from utils.smpl_utils import get_pose
+
+for gender in ["MALE", "FEMALE", "NEUTRAL"]:
+    smpl = SMPL(model_path="../../smpl_data", gender='MALE', batch_size=1)
+    poses = np.zeros((1, 24, 3))
+    with torch.no_grad():
+        A = get_pose(smpl, body_pose=torch.tensor(poses[:, 1:]).float(),
+                     global_orient=torch.tensor(poses[:, 0:1, :]).float())
+        A[:, :, :3, 3] = A[:, :, :3, 3] - A[:, [1, 2], :3, 3].mean(dim=1, keepdim=True)
+        A = A.numpy()[0]
+
+    np.save(f'../smpl_data/{gender.lower()}_canonical.npy', A)
