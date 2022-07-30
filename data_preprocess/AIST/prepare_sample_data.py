49adb2d5886dbb44793defe49ac135c307fbab65
nogu-atsu <noguchi@mi.t.u-tokyo.ac.jp>
2022-07-31 03:07:50 +0900

    :sparkles: aist sample data

---
+++ data_preprocess/AIST/prepare_sample_data.py
@@ -0,0 +1,139 @@
+import argparse
+import glob
+import math
+import os
+import pickle
+import sys
+
+import cv2
+import numpy as np
+import torch
+from aist_plusplus.loader import AISTDataset
+from smplx.body_models import SMPL
+from tqdm import tqdm
+
+sys.path.append("../")
+from dependencies.smpl_utils import get_pose
+from data_preprocess.utils import get_bone_length, SMPL_PARENTS
+
+
+def preprocess_intrinsic(intrinsic, rot, trans, pose):
+    if algo == "resize":
+        intri = intrinsic.copy()
+        intri[:2] /= save_scale
+        return intri
+    elif algo == "aligned_crop":
+        focal_length = (intrinsic[0, 0] + intrinsic[1, 1]) / 2
+        _crop_size = int(crop_size * focal_length / standard_focal_length)
+        _crop_size = _crop_size // 2 * 2
+        joint_translation = pose[:, :3, 3:]  # (n_parts, 3, 1)
+        pose_3d = np.matmul(rot[None], joint_translation) + trans[None]  # (n_parts, 3, 1)
+        pose_2d = np.matmul(intrinsic[None], pose_3d)
+        pose_2d = pose_2d[:, :2, 0] / pose_2d[:, 2:, 0]  # (n_parts, 2)
+
+        spine = pose_2d[0]  # (B, 2)
+        x1 = math.floor(spine[0]) - _crop_size // 2
+        y1 = math.floor(spine[1]) - _crop_size // 2
+
+        # camera intrinsic
+        intri = intrinsic.copy()
+        intri[:2, 2] -= np.array([x1, y1])
+        intri[:2] /= (_crop_size / save_size)
+        return intri
+
+    else:
+        raise ValueError()
+
+
+def save_sample_data(person_id):
+    all_video_path = sorted(glob.glob(f"{VIDEO_DIR}/*_d{person_id:0>2}_*.mp4"))
+    print(len(all_video_path))
+
+    sample_data = []
+
+    for video_path in tqdm(all_video_path[:5]):
+        video_name = os.path.splitext(os.path.basename(video_path))[0]
+        seq_name, view = AISTDataset.get_seq_name(video_name)
+        view_idx = AISTDataset.VIEWS.index(view)
+        env_name = aist_dataset.mapping_seq2env[seq_name]
+        cgroup = AISTDataset.load_camera_group(aist_dataset.camera_dir, env_name)
+
+        camera_mat = cgroup.cameras[view_idx].matrix
+        rmat = cv2.Rodrigues(cgroup.cameras[view_idx].rvec)[0]
+        tvec = cgroup.cameras[view_idx].tvec[:, None]
+
+        smpl_poses, smpl_scaling, smpl_trans = AISTDataset.load_motion(aist_dataset.motion_dir, seq_name)
+        poses = smpl_poses.reshape(-1, 24, 3)
+
+        with torch.no_grad():
+            smpl_pose = get_pose(smpl, body_pose=torch.tensor(poses[:1, 1:]).float(),
+                                 global_orient=torch.tensor(poses[:1, 0:1, :]).float())
+            smpl_pose = smpl_pose.numpy()
+            smpl_pose[:, :, :3, 3] *= smpl_scaling
+            smpl_pose[:, :, :3, 3] += smpl_trans[:1, None]
+            smpl_pose[:, :, :3, 3] /= 100
+
+            # rotate
+            smpl_pose[:, :, :3, :3] = np.matmul(rmat,
+                                                smpl_pose[:, :, :3, :3])
+            smpl_pose[:, :, :3, 3:] = np.matmul(rmat, smpl_pose[:, :, :3, 3:]) + tvec
+
+        intri = preprocess_intrinsic(camera_mat, rmat, tvec, smpl_pose[0])
+
+        sample_data.append({
+            "pose_to_camera": smpl_pose[0],
+            "intrinsics": intri,
+            "bone_length": get_bone_length(smpl_pose[0], SMPL_PARENTS)
+        })
+
+    out_dir = f"../data/aist++"
+    os.makedirs(out_dir)
+    with open(f"{out_dir}/sample_data.pickle", "wb") as f:
+        pickle.dump(sample_data, f)
+
+    #
+    # all_video = np.concatenate(all_video, axis=0)
+    # all_processed_intrinsic = np.concatenate(all_processed_intrinsic, axis=0)
+    # all_validity = np.concatenate(all_validity, axis=0)
+    # all_rmat = np.concatenate(all_rmat, axis=0)
+    # all_tvec = np.concatenate(all_tvec, axis=0)
+    # all_smpl = np.concatenate(all_smpl, axis=0)
+    #
+    #
+    # print("num frames", len(all_video))
+    # sample_idx = np.linspace(0, len(all_video) - 1, N_PER_PERSON, dtype="int")
+    #
+    # all_video = all_video[sample_idx]
+    # all_processed_intrinsic = all_processed_intrinsic[sample_idx]
+    # # all_validity = all_validity[sample_idx]
+    # all_rmat = all_rmat[sample_idx]
+    # all_tvec = all_tvec[sample_idx].copy()
+    # all_smpl = all_smpl[sample_idx].copy()
+    #
+    # # normalize
+    # all_tvec = all_tvec / 100
+    # all_smpl[:, :, :3, 3] /= 100
+    #
+    # return all_video, all_rmat, all_tvec, all_smpl, all_processed_intrinsic
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str)
+    args = parser.parse_args()
+
+    algo = "aligned_crop"
+    crop_size = 600
+    save_size = 128
+    standard_focal_length = 1500
+
+    save_scale = crop_size / save_size
+
+    VIDEO_DIR = "/data/unagi0/noguchi/dataset/aist++"
+    ANNOTATION_DIR = "/data/unagi0/noguchi/dataset/aist++/aist_plusplus_final"
+    SMPL_MODEL_PATH = "../smpl_data"
+    smpl = SMPL(model_path=SMPL_MODEL_PATH, gender='MALE', batch_size=1)
+    aist_dataset = AISTDataset(ANNOTATION_DIR)
+    PERSON_ID = 1
+
+    save_sample_data(PERSON_ID)
